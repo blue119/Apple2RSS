02ff2477b2bfc7c81bbdac2fe5fc3e45898d57cb
Yao-Po Wang <blue119@gmail.com>
2012-08-20 09:35:36 +0800

    file mode change to normal use

--- apple2html.sh
+++ apple2html.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cd ${HOME}/iLab/AppleNews
+# cd ${HOME}/iLab/AppleNews
 # python apple2html.py
-source ~/.pythonbrew/etc/bashrc
-pythonbrew use Python-2.7.2
+# source ~/.pythonbrew/etc/bashrc
+# pythonbrew use Python-2.7.2
 python apple2html.py
